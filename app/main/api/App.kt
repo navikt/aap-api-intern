2736a9b7ad9289a828a7e1aa2861bf7d6c03f930
yongbinkwon <yong.bin.kwon@nav.no>
2024-11-13 14:52:30 +0100

    fjern ubrukte imports

--- app/main/api/App.kt
+++ app/main/api/App.kt
@@ -1,39 +1,23 @@
 package api
 
 import api.arena.ArenaoppslagRestClient
-import com.auth0.jwk.JwkProviderBuilder
 import com.papsign.ktor.openapigen.model.info.InfoModel
 import com.papsign.ktor.openapigen.route.apiRouting
 import io.ktor.http.*
-import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
-import io.ktor.server.auth.jwt.*
 import io.ktor.server.engine.*
-import io.ktor.server.metrics.micrometer.*
 import io.ktor.server.netty.*
-import io.ktor.server.plugins.callid.*
-import io.ktor.server.plugins.calllogging.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.cors.routing.*
-import io.ktor.server.plugins.swagger.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.Tag
-import io.micrometer.core.instrument.binder.logging.LogbackMetrics
 import io.micrometer.prometheusmetrics.PrometheusConfig
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
 import no.nav.aap.komponenter.httpklient.httpclient.tokenprovider.azurecc.AzureConfig
-import no.nav.aap.komponenter.httpklient.json.DefaultJsonMapper
 import no.nav.aap.komponenter.server.AZURE
 import no.nav.aap.komponenter.server.commonKtorModule
 import org.slf4j.LoggerFactory
-import org.slf4j.event.Level
-import java.net.URI
-import java.util.*
-import java.util.concurrent.TimeUnit
 
 private val logger = LoggerFactory.getLogger("App")
 
