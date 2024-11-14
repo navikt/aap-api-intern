648baea9abd66f22f0140ae9063891a3f1473641
yongbinkwon <yong.bin.kwon@nav.no>
2024-11-14 14:11:04 +0100

    legg til tags og descriptions for endepunkt i swagger doc

--- app/main/api/Routes.kt
+++ app/main/api/Routes.kt
@@ -4,11 +4,14 @@ import api.arena.ArenaoppslagRestClient
 import api.perioder.PerioderInkludert11_17Response
 import api.perioder.PerioderResponse
 import api.perioder.SakStatus
+import com.papsign.ktor.openapigen.APITag
 import com.papsign.ktor.openapigen.annotations.parameters.HeaderParam
+import com.papsign.ktor.openapigen.route.info
 import com.papsign.ktor.openapigen.route.path.normal.NormalOpenAPIRoute
 import com.papsign.ktor.openapigen.route.path.normal.post
 import com.papsign.ktor.openapigen.route.response.respond
 import com.papsign.ktor.openapigen.route.route
+import com.papsign.ktor.openapigen.route.tag
 import io.ktor.http.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -31,37 +34,51 @@ data class CallIdHeader(
     }
 }
 
+enum class Tag(override val description: String) : APITag {
+    Perioder("For å hente perioder med AAP"),
+    Saker("For å hente AAP-saker")
+}
+
 fun NormalOpenAPIRoute.api(arena: ArenaoppslagRestClient, httpCallCounter: PrometheusMeterRegistry) {
 
-    route("/perioder") {
-        post<CallIdHeader, PerioderResponse, InternVedtakRequest> { callIdHeader, requestBody ->
-            httpCallCounter.httpCallCounter("/perioder").increment()
-            val callId = callIdHeader.callId() ?: UUID.randomUUID().also {
-                logger.info("CallID ble ikke gitt på kall mot: /perioder")
+    tag(Tag.Perioder) {
+        route("/perioder") {
+            post<CallIdHeader, PerioderResponse, InternVedtakRequest>(
+                info(description = "Henter perioder med vedtak for en person innen gitte datointerval")
+            ) { callIdHeader, requestBody ->
+                httpCallCounter.httpCallCounter("/perioder").increment()
+                val callId = callIdHeader.callId() ?: UUID.randomUUID().also {
+                    logger.info("CallID ble ikke gitt på kall mot: /perioder")
+                }
+
+                respond(arena.hentPerioder(callId, requestBody))
             }
 
-            respond(arena.hentPerioder(callId, requestBody))
-        }
+            route("/aktivitetfase").post<CallIdHeader, PerioderInkludert11_17Response, InternVedtakRequest>(
+                info(description = "Henter perioder med vedtak (inkl. aktivitetsfase) for en person innen gitte datointerval")
+            ) { callIdHeader, requestBody ->
+                httpCallCounter.httpCallCounter("/perioder/aktivitetfase").increment()
+                val callId = callIdHeader.callId() ?: UUID.randomUUID().also {
+                    logger.info("CallID ble ikke gitt på kall mot: /perioder/aktivitetfase")
+                }
 
-        route("/aktivitetfase").post<CallIdHeader, PerioderInkludert11_17Response, InternVedtakRequest> { callIdHeader, requestBody ->
-            httpCallCounter.httpCallCounter("/perioder/aktivitetfase").increment()
-            val callId = callIdHeader.callId() ?: UUID.randomUUID().also {
-                logger.info("CallID ble ikke gitt på kall mot: /perioder/aktivitetfase")
+                respond(arena.hentPerioderInkludert11_17(callId, requestBody))
             }
-
-            respond(arena.hentPerioderInkludert11_17(callId, requestBody))
         }
     }
 
-    route("/sakerByFnr").post<CallIdHeader, List<SakStatus>, SakerRequest> { callIdHeader, requestBody ->
-        httpCallCounter.httpCallCounter("/sakerByFnr").increment()
-        val callId = callIdHeader.callId() ?: UUID.randomUUID().also {
-            logger.info("CallID ble ikke gitt på kall mot: /sakerByFnr")
+    tag(Tag.Saker) {
+        route("/sakerByFnr").post<CallIdHeader, List<SakStatus>, SakerRequest>(
+            info(description = "Henter saker for en person")
+        ) { callIdHeader, requestBody ->
+            httpCallCounter.httpCallCounter("/sakerByFnr").increment()
+            val callId = callIdHeader.callId() ?: UUID.randomUUID().also {
+                logger.info("CallID ble ikke gitt på kall mot: /sakerByFnr")
+            }
+
+            respond(arena.hentSakerByFnr(callId, requestBody))
         }
-
-        respond(arena.hentSakerByFnr(callId, requestBody))
     }
-
 }
 
 fun Routing.actuator(prometheus: PrometheusMeterRegistry) {
