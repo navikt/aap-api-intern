4ef9d4ac14308a6d2954577221c26e6a55024663
Fredrik Meyer <fredrik.meyer@nav.no>
2024-11-26 11:37:50 +0100

    Fjern usikker avhengighet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,9 @@ dependencies {
     implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-server-metrics-micrometer:$ktorVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    constraints {
+        implementation("io.netty:netty-common:4.1.115.Final")
+    }
     implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
     implementation("io.ktor:ktor-server-webjars:$ktorVersion")
     implementation("io.ktor:ktor-server-call-id:$ktorVersion")
