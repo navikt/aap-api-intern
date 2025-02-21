de6a4a4441d7982ba47f1704e41464032ea6be04
VHollund <vetle.hollund@nav.no>
2025-02-21 10:27:04 +0100

    fjernet ubrukt envVar

--- app/main/api/Config.kt
+++ app/main/api/Config.kt
@@ -33,7 +33,6 @@ data class PostgresConfig(
     val username: String = getEnvVar("NAIS_DATABASE_API_INTERN_API_USERNAME"),
     val password: String = getEnvVar("NAIS_DATABASE_API_INTERN_API_PASSWORD"),
     val database: String = getEnvVar("NAIS_DATABASE_API_INTERN_API_DATABASE"),
-    val sslmode: String = getEnvVar("NAIS_DATABASE_API_INTERN_API_SSLMODE"),
     val url: String = getEnvVar("NAIS_DATABASE_API_INTERN_API_JDBC_URL"),
         //"jdbc:postgresql://${host}:${port}/${database}?sslmode=${sslmode}",
     val driver: String = "org.postgresql.Driver",
