944683f6796cbeb540514c37263808043376cbdf
VHollund <vetle.hollund@nav.no>
2025-02-25 13:19:29 +0100

    info modul på insert endepunkt

--- app/main/api/kelvin/Route.kt
+++ app/main/api/kelvin/Route.kt
@@ -2,24 +2,19 @@ package api.kelvin
 
 import api.Tag
 import api.postgres.MeldekortPerioderRepository
-import com.papsign.ktor.openapigen.model.info.InfoModel
-import com.papsign.ktor.openapigen.modules.RouteOpenAPIModule
 import com.papsign.ktor.openapigen.route.path.normal.NormalOpenAPIRoute
 import com.papsign.ktor.openapigen.route.tag
 import com.papsign.ktor.openapigen.route.info
-import com.papsign.ktor.openapigen.route.path.normal.post
 import com.papsign.ktor.openapigen.route.response.respond
 import com.papsign.ktor.openapigen.route.route
 import io.ktor.http.*
-import io.ktor.server.auth.*
-import io.ktor.server.auth.jwt.*
-import io.ktor.util.pipeline.*
 import no.nav.aap.komponenter.dbconnect.transaction
 import no.nav.aap.komponenter.type.Periode
 import no.nav.aap.tilgang.AuthorizationBodyPathConfig
 import no.nav.aap.tilgang.authorizedPost
 import tilgang.Operasjon
 import javax.sql.DataSource
+import com.papsign.ktor.openapigen.route.info
 
 fun NormalOpenAPIRoute.dataInsertion(dataSource: DataSource) {
     tag(Tag.Insertion) {
@@ -29,7 +24,8 @@ fun NormalOpenAPIRoute.dataInsertion(dataSource: DataSource) {
                     operasjon = Operasjon.SE,
                     applicationsOnly = true,
                     applicationRole = "add-data",
-                )
+                ),
+                modules = listOf(info("Legg inn meldekortperioder", "Legg inn meldekortperioder for en person. Endepunktet kan kun brukes av behandlingsflyt")).toTypedArray(),
             ) { _, body ->
                 val perioder = dataSource.transaction { connection ->
                     val meldekortPerioderRepository = MeldekortPerioderRepository(connection)
