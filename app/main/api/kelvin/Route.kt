2606f433ddb1810c8ae2abb4c2a1bc31dbf7714c
VHollund <vetle.hollund@nav.no>
2025-02-27 15:29:32 +0100

    byttet retur på insert

--- app/main/api/kelvin/Route.kt
+++ app/main/api/kelvin/Route.kt
@@ -8,17 +8,16 @@ import com.papsign.ktor.openapigen.route.info
 import com.papsign.ktor.openapigen.route.response.respond
 import com.papsign.ktor.openapigen.route.route
 import io.ktor.http.*
+import io.ktor.server.response.*
 import no.nav.aap.komponenter.dbconnect.transaction
 import no.nav.aap.komponenter.type.Periode
 import no.nav.aap.tilgang.AuthorizationBodyPathConfig
 import no.nav.aap.tilgang.authorizedPost
 import tilgang.Operasjon
 import javax.sql.DataSource
-import com.papsign.ktor.openapigen.route.info
-
 fun NormalOpenAPIRoute.dataInsertion(dataSource: DataSource) {
     route("/api/insert") {
-        route("meldeperioder").authorizedPost<Unit, List<Periode>, MeldekortPerioderDTO>(
+        route("/meldeperioder").authorizedPost<Unit, Unit, MeldekortPerioderDTO>(
             routeConfig = AuthorizationBodyPathConfig(
                 operasjon = Operasjon.SE,
                 applicationsOnly = true,
@@ -31,11 +30,11 @@ fun NormalOpenAPIRoute.dataInsertion(dataSource: DataSource) {
                 )
             ).toTypedArray(),
         ) { _, body ->
-            val perioder = dataSource.transaction { connection ->
+            dataSource.transaction { connection ->
                 val meldekortPerioderRepository = MeldekortPerioderRepository(connection)
                 meldekortPerioderRepository.lagreMeldekortPerioder(body.personIdent, body.meldekortPerioder)
             }
-            respond(perioder, HttpStatusCode.OK)
+            pipeline.call.respond(HttpStatusCode.OK)
         }
     }
 }
