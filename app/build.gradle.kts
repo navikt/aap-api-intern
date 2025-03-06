6a6a9c54f40a432b3828c55db3f96e4cd99103cb
VHollund <vetle.hollund@nav.no>
2025-03-06 10:37:27 +0100

    repo ting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,22 @@ application {
     mainClass.set("api.AppKt")
 }
 
+repositories {
+    mavenCentral()
+    maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+    maven {
+        name = "GitHubPackages"
+        url = uri("https://maven.pkg.github.com/navikt/behandlingsflyt")
+        credentials {
+            username = "x-access-token"
+            password = (project.findProperty("githubPassword")
+                ?: System.getenv("GITHUB_PASSWORD")
+                ?: System.getenv("GITHUB_TOKEN")
+                ?: error("")).toString()
+        }
+    }
+}
+
 val aapLibVersion = "5.0.25"
 val komponenterVersjon = "1.0.159"
 val ktorVersion = "3.1.1"
