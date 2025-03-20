411f369ffe1fbbd448bc1d5777b8c41ded64100c
VHollund <vetle.hollund@nav.no>
2025-03-20 13:54:46 +0100

    setter sak status til løpende eller avsluttet ut fra status på behandling

--- app/main/api/postgres/BehandlingsRepository.kt
+++ app/main/api/postgres/BehandlingsRepository.kt
@@ -201,7 +201,7 @@ class BehandlingsRepository(private val connection: DBConnection) {
                         periode, VedtakUtenUtbetaling(
                             vedtaksId = behandling.behandlingsId,
                             dagsats = right.verdi.dagsats,
-                            status = behandling.sak.status.toString(),
+                            status = if(behandling.behandlingStatus == no.nav.aap.behandlingsflyt.kontrakt.behandling.Status.IVERKSETTES || behandling.behandlingStatus == no.nav.aap.behandlingsflyt.kontrakt.behandling.Status.AVSLUTTET){Status.LØPENDE} else {Status.UTREDES}.toString(),
                             saksnummer = behandling.sak.saksnummer,
                             vedtaksdato = behandling.vedtaksDato.format(DateTimeFormatter.ISO_LOCAL_DATE),
                             vedtaksTypeKode = "",
@@ -216,7 +216,7 @@ class BehandlingsRepository(private val connection: DBConnection) {
                         )
                     )
                 }
-            ).komprimer().map { it.verdi }
+            ).komprimer().map { it.verdi }.filter { it.status == Status.LØPENDE.toString() || it.status == Status.AVSLUTTET.toString() }
         }
 
         return Medium(vedtak)
