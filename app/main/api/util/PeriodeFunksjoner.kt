effc9f4794732a8d038cfbe07c1edaf7cc83c4af
VHollund <vetle.hollund@nav.no>
2025-03-27 10:35:23 +0100

    oppdatert perioder med aap

--- app/main/api/util/PeriodeFunksjoner.kt
+++ app/main/api/util/PeriodeFunksjoner.kt
@@ -7,12 +7,8 @@ import no.nav.aap.komponenter.type.Periode
 
 fun perioderMedAAp(input:List<DatadelingDTO>, interval:Periode):List<no.nav.aap.api.intern.Periode>{
     return input.flatMap { sak ->
-        sak.tilkjent
-            .filter { it.gradering > 0 }
-            .map { Segment(Periode(it.tilkjentFom, it.tilkjentTom),null) }
-            .let (::Tidslinje)
-            .filter { interval.overlapper(it.periode) }
-            .komprimer()
-            .map { no.nav.aap.api.intern.Periode(it.periode.fom, it.periode.tom) }
+        sak.rettighetsTypeTidsLinje.map { segment ->
+            no.nav.aap.api.intern.Periode(segment.fom, segment.tom)
+            }
     }
 }
