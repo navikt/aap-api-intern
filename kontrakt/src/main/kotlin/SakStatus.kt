c6d9d6554b3c2ac59ec5c62f0d8bc99a2f09b859
Fredrik Meyer <fredrik.meyer@nav.no>
2025-03-31 16:04:40 +0200

    Legg på validering på Periode-objektet

--- kontrakt/src/main/kotlin/SakStatus.kt
+++ kontrakt/src/main/kotlin/SakStatus.kt
@@ -2,21 +2,27 @@ package no.nav.aap.api.intern
 
 import java.time.LocalDate
 
-data class Periode(val fraOgMedDato: LocalDate?, val tilOgMedDato: LocalDate?)
+data class Periode(val fraOgMedDato: LocalDate?, val tilOgMedDato: LocalDate?) {
+    init {
+        if (fraOgMedDato != null && tilOgMedDato != null && fraOgMedDato != tilOgMedDato) {
+            require(fraOgMedDato.isBefore(tilOgMedDato)) { "Fra og med-dato må være før til og med-dato: $fraOgMedDato, $tilOgMedDato" }
+        }
+    }
+}
 
 data class SakStatus(
     val sakId: String,
     val statusKode: Status,
     val periode: Periode,
-    val kilde:Kilde = Kilde.ARENA
+    val kilde: Kilde = Kilde.ARENA
 )
 
-enum class Kilde{
+enum class Kilde {
     ARENA,
     KELVIN
 }
 
-enum class Status{
+enum class Status {
     AVSLU,
     FORDE,
     GODKJ,
