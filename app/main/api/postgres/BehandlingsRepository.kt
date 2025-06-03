65ef80aa68f97501aa775d3b84201f7c014dd2cc
Fredrik Meyer <fredrik.meyer@nav.no>
2025-06-03 08:22:59 +0200

    Fikset kompileringsfeil på main

--- app/main/api/postgres/BehandlingsRepository.kt
+++ app/main/api/postgres/BehandlingsRepository.kt
@@ -210,7 +210,11 @@ class BehandlingsRepository(private val connection: DBConnection) {
                         VedtakUtenUtbetalingUtenPeriode(
                             vedtakId = behandling.behandlingsReferanse,
                             dagsats = right?.verdi?.dagsats ?: 0,
-                            status = utledVedtakStatus(behandling.behandlingStatus, periode),
+                            status = utledVedtakStatus(
+                                behandling.behandlingStatus,
+                                behandling.sak.status,
+                                periode
+                            ),
                             saksnummer = behandling.sak.saksnummer,
                             vedtaksdato = behandling.vedtaksDato,
                             vedtaksTypeKode = null,
@@ -259,7 +263,7 @@ class BehandlingsRepository(private val connection: DBConnection) {
                                         utbetaling.periode.tom
                                     ),
                                     // TODO: bør hente korrekt beløp på samme måte som i behandlingsflyt
-                                    belop = ((utbetaling.verdi.dagsats+utbetaling.verdi.barnetillegg.toInt()) * utbetaling.verdi.gradering) / 100 * weekdaysBetween(
+                                    belop = ((utbetaling.verdi.dagsats + utbetaling.verdi.barnetillegg.toInt()) * utbetaling.verdi.gradering) / 100 * weekdaysBetween(
                                         utbetaling.periode.fom,
                                         utbetaling.periode.tom
                                     ),
