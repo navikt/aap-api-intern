5fed7a5d67308c613c96a7262e22cbb8de085766
Steffen Hageland <steffen.hageland@nav.no>
2025-07-15 12:48:59 +0200

    Legge til resillience4j

--- app/main/api/App.kt
+++ app/main/api/App.kt
@@ -6,6 +6,7 @@ import api.kelvin.dataInsertion
 import api.pdl.IPdlClient
 import api.pdl.PdlClient
 import api.postgres.initDatasource
+import api.util.registerCircuitBreakerMetrics
 import com.papsign.ktor.openapigen.model.info.ContactModel
 import com.papsign.ktor.openapigen.model.info.InfoModel
 import com.papsign.ktor.openapigen.route.apiRouting
@@ -52,7 +53,9 @@ fun Application.api(
     arenaRestClient: IArenaoppslagRestClient = ArenaoppslagRestClient(config.arenaoppslag, config.azure),
     pdlClient: IPdlClient = PdlClient(),
 ) {
+
     Migrering.migrate(datasource)
+    registerCircuitBreakerMetrics(prometheus)
 
     install(StatusPages) {
         exception<Throwable> { call, cause ->
