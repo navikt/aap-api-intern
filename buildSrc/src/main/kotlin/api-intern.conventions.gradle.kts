15264c520c3605c9785e2cc681c5fcfe73d09f57
Per Henden <per.christian.henden@nav.no>
2025-09-16 10:45:23 +0200

    Gradle: korriger navngiving på jar-filer, korriger java-versjon setting

--- buildSrc/src/main/kotlin/api-intern.conventions.gradle.kts
+++ buildSrc/src/main/kotlin/api-intern.conventions.gradle.kts
@@ -1,4 +1,4 @@
-// Felles kode for alle build.gradle.kts filer som laster api-intern.conventions pluginen
+// Felles kode for alle build.gradle.kts filer som denne conventions pluginen
 
 plugins {
     id("org.jetbrains.kotlin.jvm")
@@ -12,16 +12,34 @@ tasks {
         useJUnitPlatform()
         maxParallelForks = Runtime.getRuntime().availableProcessors() / 2
     }
+
+    (tasks.findByName("distTar") as? Tar)?.apply {
+        // Bruk et unikt navn for jar-filen til distTar, for å unngå navnekollisjoner i multi-modul prosjekt,
+        // slik at vi ikke bruker samme navn, feks. "kontrakt.jar" "api.jar" i flere moduler.
+        // Dette unngår feil av typen "Entry <name>.jar is a duplicate but no duplicate handling strategy has been set"
+        archiveBaseName.set("${rootProject.name}-${project.name}")
+    }
+
 }
 
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
     compilerOptions {
         jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
         apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_2)
         languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_2)
     }
 }
+
+// Pass på at når vi kaller JavaExec eller Test tasks så bruker vi samme JVM som vi kompilerer med
+val toolchainLauncher = javaToolchains.launcherFor {
+    languageVersion.set(JavaLanguageVersion.of(21))
+}
+tasks.withType<Test>().configureEach { javaLauncher.set(toolchainLauncher) }
+tasks.withType<JavaExec>().configureEach { javaLauncher.set(toolchainLauncher) }
+
 
 kotlin.sourceSets["main"].kotlin.srcDirs("main")
 kotlin.sourceSets["test"].kotlin.srcDirs("test")
