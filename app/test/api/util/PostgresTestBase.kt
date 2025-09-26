6284e24a474dfde2410350fee59fed93f052201f
Per Henden <per.christian.henden@nav.no>
2025-09-26 14:26:48 +0200

    Skru på debug logging for postgres og hikari under test

--- app/test/api/util/PostgresTestBase.kt
+++ app/test/api/util/PostgresTestBase.kt
@@ -3,11 +3,24 @@ package api.util
 import no.nav.aap.komponenter.dbconnect.transaction
 import no.nav.aap.komponenter.dbmigrering.Migrering
 import org.junit.jupiter.api.BeforeEach
+import org.slf4j.bridge.SLF4JBridgeHandler
+import java.util.logging.Level
 import javax.sql.DataSource
 
 abstract class PostgresTestBase(val dataSource: DataSource) {
     init {
         Migrering.migrate(dataSource, false)
+
+        enablePostgresDebugLogs()
+    }
+
+    private fun enablePostgresDebugLogs() {
+        // Postgres uses java.util.logging, so we need to bridge it to SLF4J
+        if (!SLF4JBridgeHandler.isInstalled()) {
+            SLF4JBridgeHandler.install()
+        }
+        // Override the log level from postgres-jdbc's default
+        java.util.logging.Logger.getLogger("org.postgresql").level = Level.FINE
     }
 
     @BeforeEach
