7f9656a6a3c93681cfbbad99382851f80a4100b8
Per Henden <per.christian.henden@nav.no>
2025-11-03 13:58:50 +0100

    fix: gradle-oppsett som i behandlingsflyt
    Følg samme mønster som behandlingsflyt i de andre appene som publiserer jar-filer
    
    AAP-1627
    
    AAP-1627

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,8 +12,8 @@ dependencyResolutionManagement {
     // Felles for alle gradle prosjekter i repoet
     @Suppress("UnstableApiUsage")
     repositories {
-        mavenCentral()
         maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+        mavenCentral()
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/navikt/behandlingsflyt")
