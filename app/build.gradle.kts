751a94584b94eafeeff1a3c5fb8e083010c91a59
Fredrik Meyer <fredrik.meyer@nav.no>
2025-11-11 10:17:02 +0100

    Logstash-logback-encoder v9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.20.1")
     implementation("com.nimbusds:nimbus-jose-jwt:10.6")
     implementation("io.micrometer:micrometer-registry-prometheus:1.16.0")
-    implementation("net.logstash.logback:logstash-logback-encoder:8.1")
+    implementation("net.logstash.logback:logstash-logback-encoder:9.0")
     implementation("io.prometheus:prometheus-metrics-tracer-initializer:1.4.2")
 
     implementation("com.zaxxer:HikariCP:7.0.2")
