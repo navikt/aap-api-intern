d19b596f1cbe3b6a1c1480baf59157c3af06ae8b
Per Henden <per.christian.henden@nav.no>
2026-01-22 12:40:34 +0100

    Fjern unødvendig deklarasjon

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,14 +7,9 @@ plugins {
     id("aap.conventions")
 }
 
-subprojects {
-    // no-op; just ensuring subprojects are configured
-}
-
 // Call the tasks of the subprojects
 for (taskName in listOf<String>("clean", "build", "check")) {
     tasks.named(taskName) {
         dependsOn(subprojects.map { it.path + ":$taskName" })
     }
 }
-
