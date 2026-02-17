90041d7a4e2aa2df17bd69b76cd96d133a231b12
Fredrik Meyer <fredrik.meyer@nav.no>
2026-02-17 16:00:28 +0100

    Need dep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     constraints {
         implementation("commons-codec:commons-codec:1.21.0")
     }
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
     testImplementation("org.assertj:assertj-core:3.27.7")
     testImplementation("com.nimbusds:nimbus-jose-jwt:10.7")
     testImplementation(kotlin("test"))
