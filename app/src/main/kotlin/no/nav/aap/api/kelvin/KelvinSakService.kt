e8e15a748865dadddf423c214315fbfbe2cb7741
Fredrik Meyer <fredrik.meyer@nav.no>
2026-03-10 09:12:50 +0100

    Bruk mindre respons for meldekort-backend

--- app/src/main/kotlin/no/nav/aap/api/kelvin/KelvinSakService.kt
+++ app/src/main/kotlin/no/nav/aap/api/kelvin/KelvinSakService.kt
@@ -2,6 +2,7 @@ package no.nav.aap.api.kelvin
 
 import no.nav.aap.api.intern.NåværendeEnhet
 import no.nav.aap.api.intern.SakStatus
+import no.nav.aap.api.intern.SakStatusMeldekortbackend
 import no.nav.aap.api.postgres.SakStatusRepository
 
 class KelvinSakService(private val sakStatusRepository: SakStatusRepository) {
@@ -12,18 +13,26 @@ class KelvinSakService(private val sakStatusRepository: SakStatusRepository) {
     fun hentSakStatus(identer: List<String>): List<SakStatus> {
 
         return identer.flatMap { ident ->
-            val (enhetinfo, saksnummer) = OppgaveGateway.hentEnhetForPerson(ident) ?: Pair(null, null)
+            val (enhetinfo, saksnummer) = OppgaveGateway.hentEnhetForPerson(ident) ?: Pair(
+                null,
+                null
+            )
 
             sakStatusRepository.hentSakStatus(ident)
-                .map { if (enhetinfo != null && it.sakId == saksnummer) it.copy(enhet = NåværendeEnhet(
-                    oversendtDato = enhetinfo.oversendtDato,
-                    oppgaveKategori = enhetinfo.oppgaveKategori,
-                    enhet =  enhetinfo.enhet,
-                )) else it }
+                .map {
+                    if (enhetinfo != null && it.sakId == saksnummer) it.copy(
+                        enhet = NåværendeEnhet(
+                            oversendtDato = enhetinfo.oversendtDato,
+                            oppgaveKategori = enhetinfo.oppgaveKategori,
+                            enhet = enhetinfo.enhet,
+                        )
+                    ) else it
+                }
         }
     }
 
-    fun hentSakStatus(ident: String): List<SakStatus> {
-        return hentSakStatus(listOf(ident))
+    fun hentSakStatus(ident: String): List<SakStatusMeldekortbackend> {
+        return sakStatusRepository.hentSakStatus(ident)
+            .map { SakStatusMeldekortbackend(it.kilde, it.periode, it.sakId) }
     }
 }
