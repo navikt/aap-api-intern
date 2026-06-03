517ea99b18bdab67e8551c72168f7b8629dfe795
Fredrik Meyer <fredrik.meyer@nav.no>
2026-06-03 08:26:04 +0200

    Bruk hjelpemetode som klipper korrekt (#800)
    * Bruk hjelpemetode som klipper korrekt
    
    * Begrens med Tid.MIN
    
    * Oppdater test
    
    * Penere if
    
    * Trekk ut i metode

--- app/src/main/kotlin/no/nav/aap/api/kelvin/VedtakService.kt
+++ app/src/main/kotlin/no/nav/aap/api/kelvin/VedtakService.kt
@@ -1,22 +1,18 @@
 package no.nav.aap.api.kelvin
 
 import com.papsign.ktor.openapigen.annotations.properties.description.Description
-import java.math.BigDecimal
-import java.time.Clock
-import java.time.DayOfWeek
-import java.time.LocalDate
-import no.nav.aap.api.intern.Kilde
-import no.nav.aap.api.intern.Maksimum
-import no.nav.aap.api.intern.Medium
-import no.nav.aap.api.intern.UtbetalingMedMer
-import no.nav.aap.api.intern.Vedtak
-import no.nav.aap.api.intern.VedtakUtenUtbetaling
+import no.nav.aap.api.intern.*
 import no.nav.aap.api.postgres.BehandlingsRepository
 import no.nav.aap.behandlingsflyt.kontrakt.sak.Status
 import no.nav.aap.komponenter.tidslinje.JoinStyle
 import no.nav.aap.komponenter.tidslinje.Segment
+import no.nav.aap.komponenter.tidslinje.Tidslinje
 import no.nav.aap.komponenter.tidslinje.orEmpty
 import no.nav.aap.komponenter.type.Periode
+import java.math.BigDecimal
+import java.time.Clock
+import java.time.DayOfWeek
+import java.time.LocalDate
 
 class VedtakService(
     private val behandlingsRepository: BehandlingsRepository,
@@ -65,7 +61,7 @@ class VedtakService(
                         vedtakUtenUtbetalingUtenPeriode.arenavedtak?.vedtaksvariant?.typeKode
                             ?: if (behandling.nyttVedtak) "O" else "E"
                     val tilkjentYtelseTidslinje = right?.verdi.orEmpty()
-                    Segment(
+                    val segment = Segment(
                         periode,
                         Vedtak(
                             vedtakId = vedtakUtenUtbetalingUtenPeriode.vedtakId,
@@ -85,33 +81,13 @@ class VedtakService(
                                 ?: 0,
                             vedtaksTypeKode = vedtaksTypeKode,
                             vedtaksTypeNavn = null,
-                            utbetaling = tilkjentYtelseTidslinje.filter {
-                                it.periode.tom.isBefore(LocalDate.now(clock)) || it.periode.tom.isEqual(
-                                    LocalDate.now(clock)
-                                )
-                            }.komprimer().segmenter().map { utbetaling ->
-                                UtbetalingMedMer(
-                                    reduksjon = null,
-                                    utbetalingsgrad = utbetaling.verdi.gradering,
-                                    periode = no.nav.aap.api.intern.Periode(
-                                        utbetaling.periode.fom,
-                                        utbetaling.periode.tom
-                                    ),
-                                    // TODO: bør hente korrekt beløp på samme måte som i behandlingsflyt
-                                    belop = ((utbetaling.verdi.dagsats + utbetaling.verdi.barnetillegg.toInt()) * utbetaling.verdi.gradering) / 100 * weekdaysBetween(
-                                        utbetaling.periode.fom,
-                                        utbetaling.periode.tom
-                                    ),
-                                    dagsats = utbetaling.verdi.dagsats * utbetaling.verdi.gradering / 100,
-                                    barnetillegg = utbetaling.verdi.gradertBarnetillegg()
-                                        .toInt()
-                                )
-                            },
+                            utbetaling = hentTilkjentYtelseForPeriode(tilkjentYtelseTidslinje, periode),
                             kildesystem = vedtakUtenUtbetalingUtenPeriode.kildesystem,
                             samordningsId = vedtakUtenUtbetalingUtenPeriode.samordningsId,
                             opphorsAarsak = vedtakUtenUtbetalingUtenPeriode.opphorsAarsak
                         )
                     )
+                    segment
                 }
             ).komprimer().segmenter().map { it.verdi }
                 .filter { it.status == Status.LØPENDE.toString() || it.status == Status.AVSLUTTET.toString() }
@@ -119,6 +95,34 @@ class VedtakService(
 
         return Maksimum(vedtak)
     }
+
+    private fun hentTilkjentYtelseForPeriode(
+        tilkjentYtelseTidslinje: Tidslinje<TilkjentYtelse>,
+        periode: Periode
+    ): List<UtbetalingMedMer> = tilkjentYtelseTidslinje
+        .takeUnless { LocalDate.now(clock) < periode.fom }
+        ?.begrensetTil(Periode(periode.fom, LocalDate.now(clock)))
+        .orEmpty()
+        .komprimer()
+        .segmenter()
+        .map { utbetaling ->
+            UtbetalingMedMer(
+                reduksjon = null,
+                utbetalingsgrad = utbetaling.verdi.gradering,
+                periode = no.nav.aap.api.intern.Periode(
+                    utbetaling.periode.fom,
+                    utbetaling.periode.tom
+                ),
+                // TODO: bør hente korrekt beløp på samme måte som i behandlingsflyt
+                belop = ((utbetaling.verdi.dagsats + utbetaling.verdi.barnetillegg.toInt()) * utbetaling.verdi.gradering) / 100 * weekdaysBetween(
+                    utbetaling.periode.fom,
+                    utbetaling.periode.tom
+                ),
+                dagsats = utbetaling.verdi.dagsats * utbetaling.verdi.gradering / 100,
+                barnetillegg = utbetaling.verdi.gradertBarnetillegg()
+                    .toInt()
+            )
+        }
 
 
     fun hentMediumFraKelvin(
