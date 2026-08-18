8a058e98b94804f64b6d1ba5a1d4c8779bee2dc1
Fredrik Meyer <fredrik.meyer@nav.no>
2026-08-18 09:21:36 +0200

    AAP-BAU No-op filter i kode for maksimum-spørring (#915)

--- app/src/main/kotlin/no/nav/aap/api/arena/ArenaService.kt
+++ app/src/main/kotlin/no/nav/aap/api/arena/ArenaService.kt
@@ -4,8 +4,8 @@ import io.micrometer.core.instrument.MeterRegistry
 import no.nav.aap.api.WithMetrics
 import no.nav.aap.api.intern.ArenaSakMedVedtakResponse
 import no.nav.aap.api.intern.ArenaSakOppsummering
-import no.nav.aap.api.intern.ArenaSakerResponse
 import no.nav.aap.api.intern.ArenaSakPerson
+import no.nav.aap.api.intern.ArenaSakerResponse
 import no.nav.aap.api.intern.ArenaVedtakDetaljer
 import no.nav.aap.api.intern.ArenaVedtakfakta
 import no.nav.aap.api.intern.Periode
@@ -25,15 +25,17 @@ import no.nav.aap.arenaoppslag.kontrakt.intern.SakerRequest
 import no.nav.aap.arenaoppslag.kontrakt.intern.Status
 import no.nav.aap.arenaoppslag.kontrakt.modeller.Maksimum
 import org.slf4j.LoggerFactory
+import java.time.LocalDate
 import no.nav.aap.arenaoppslag.kontrakt.apiv1.ArenaSakMedVedtakResponse as ArenaSakMedVedtakResponseV1
 import no.nav.aap.arenaoppslag.kontrakt.apiv1.SakerRequest as SakerRequestV1
 
 class ArenaService(
-    private val arena: IArenaoppslagGateway,
-    private val arenaHistorikk: IArenaoppslagGateway
+    private val arena: IArenaoppslagGateway, private val arenaHistorikk: IArenaoppslagGateway
 ) : WithMetrics {
 
     private val secureLog = LoggerFactory.getLogger("team-logs")
+
+    private val log = LoggerFactory.getLogger(javaClass)
 
     override fun registrerMetrics(registry: MeterRegistry) {
         (arena as? WithMetrics)?.registrerMetrics(registry)
@@ -41,17 +43,16 @@ class ArenaService(
     }
 
     suspend fun eksistererIAapArena(
-        callId: String,
-        personIdenter: List<String>
+        callId: String, personIdenter: List<String>
     ): PersonEksistererIAAPArena {
-        val aapHistorikkForPerson =
-            arenaHistorikk.hentPersonHarHistorikkIArena(callId, HarHistorikkRequest(personIdenter.first()))
+        val aapHistorikkForPerson = arenaHistorikk.hentPersonHarHistorikkIArena(
+            callId, HarHistorikkRequest(personIdenter.first())
+        )
         return PersonEksistererIAAPArena(aapHistorikkForPerson.harHistorikk)
     }
 
     suspend fun aktivitetfase(
-        callId: String,
-        vedtakRequest: InternVedtakRequest
+        callId: String, vedtakRequest: InternVedtakRequest
     ): PerioderInkludert11_17Response {
         val arenaSvar = arena.hentPerioderInkludert11_17(callId, vedtakRequest)
 
@@ -60,15 +61,13 @@ class ArenaService(
                 PeriodeInkludert11_17(
                     periode = periode.periode.let {
                         Periode(
-                            it.fraOgMedDato,
-                            it.tilOgMedDato
+                            it.fraOgMedDato, it.tilOgMedDato
                         )
                     },
                     aktivitetsfaseKode = periode.aktivitetsfaseKode,
                     aktivitetsfaseNavn = periode.aktivitetsfaseNavn,
                 )
-            }
-        )
+            })
     }
 
     suspend fun hentSaker(callId: String, personIdenter: List<String>): List<SakStatus.Arena> {
@@ -84,8 +83,7 @@ class ArenaService(
 
     private fun arenaSakStatusTilDomene(it: no.nav.aap.arenaoppslag.kontrakt.intern.SakStatus) =
         SakStatus.Arena(
-            sakId = it.sakId,
-            statusKode = when (it.statusKode) {
+            sakId = it.sakId, statusKode = when (it.statusKode) {
                 Status.AVSLU -> no.nav.aap.api.intern.ArenaStatus.AVSLU
                 Status.FORDE -> no.nav.aap.api.intern.ArenaStatus.FORDE
                 Status.GODKJ -> no.nav.aap.api.intern.ArenaStatus.GODKJ
@@ -96,10 +94,8 @@ class ArenaService(
                 Status.OPPRE -> no.nav.aap.api.intern.ArenaStatus.OPPRE
                 Status.REGIS -> no.nav.aap.api.intern.ArenaStatus.REGIS
                 Status.UKJENT -> no.nav.aap.api.intern.ArenaStatus.UKJENT
-            },
-            periode = Periode(
-                it.periode.fraOgMedDato,
-                it.periode.tilOgMedDato
+            }, periode = Periode(
+                it.periode.fraOgMedDato, it.periode.tilOgMedDato
             )
         )
 
@@ -108,30 +104,21 @@ class ArenaService(
     }
 
     suspend fun hentSakerForPerson(
-        callId: String,
-        personidentifikator: String
+        callId: String, personidentifikator: String
     ): ArenaSakerResponse {
         return arena.hentSakerForPerson(callId, SakerRequestV1(personidentifikator)).toResponse()
     }
 
     suspend fun hentManuellFordelingsgrunnlag(
-        callId: String,
-        personidentifikator: String
+        callId: String, personidentifikator: String
     ): ManuellFordelingsgrunnlagResponse? {
         return arena.hentManuellFordelingsgrunnlag(callId, personidentifikator)
     }
 
     suspend fun hentVedtakUtenUtbetaling(
-        callId: String,
-        vedtakRequest: InternVedtakRequest
+        callId: String, vedtakRequest: InternVedtakRequest
     ): List<InternVedtakUtenUtbetaling> {
-        return maksimum(callId, vedtakRequest).vedtak
-            .filter {
-                it.periode.fraOgMedDato == null ||
-                        it.periode.tilOgMedDato == null ||
-                        it.periode.fraOgMedDato!! <= it.periode.tilOgMedDato
-            }
-            .map { it.fraKontraktUtenUtbetaling() }
+        return maksimum(callId, vedtakRequest).vedtak.map { it.fraKontraktUtenUtbetaling() }
     }
 
     suspend fun hentVedtak(callId: String, vedtakRequest: InternVedtakRequest): List<InternVedtak> {
@@ -140,17 +127,50 @@ class ArenaService(
 
 
     private suspend fun maksimum(
-        callId: String,
-        vedtakRequest: InternVedtakRequest
+        callId: String, vedtakRequest: InternVedtakRequest
     ): Maksimum {
-        return arena.hentMaksimum(callId, vedtakRequest).let {
+        val hentMaksimum = arena.hentMaksimum(callId, vedtakRequest)
+        val medKodefilter = hentMaksimum.let {
             it.copy(vedtak = it.vedtak.filter { vedtak ->
                 // Gjenskaper filter i Arenaoppslag for å kunne
-                // https://github.com/navikt/aap-arenaoppslag/blob/d0098e8283da57f5b4bd5d853f4227d8ddcda41b/app/src/main/kotlin/no/nav/aap/arenaoppslag/database/VedtakRepository.kt#L91
+                // https://github.com/navikt/aap-arenaoppslag/blob/0506908b60c81103882386a3ed8572bb5e7d17bf/app/src/main/kotlin/no/nav/aap/arenaoppslag/database/MaksimumRepository.kt#L210
+                /*
+                           AND vedtaktypekode IN ('O', 'E', 'G', 'S')
+                           AND vedtakstatuskode IN ('IVERK', 'AVSLU')
+                           AND (fra_dato <= til_dato OR til_dato IS NULL)
+                           AND (til_dato >= ? OR til_dato IS NULL)
+                           AND fra_dato <= ?
+                 */
+                val fraOgMedDato = vedtak.periode.fraOgMedDato
+                val datoBetingelser =
+                    fraOgMedDato == null || vedtak.periode.tilOgMedDato == null || fraOgMedDato <= vedtak.periode.tilOgMedDato
+
+                val vedtaktypeBetingelser = vedtak.vedtaksTypeKode in listOf("O", "E", "G", "S")
+
+                val vedtakstatusKode = vedtak.status in listOf("IVERK", "AVSLU")
+
+                datoBetingelser and vedtaktypeBetingelser and vedtakstatusKode
+            })
+        }
+
+        // Midlertidig logg. Er denne noengang null?
+        if (hentMaksimum.vedtak.any { it.utfallkode == null }) {
+            log.warn("Maksimum-vedtak med null utfallkode.")
+        }
+
+        val original = hentMaksimum.let {
+            it.copy(vedtak = it.vedtak.filter { vedtak ->
                 val fraOgMedDato = vedtak.periode.fraOgMedDato
                 fraOgMedDato == null || vedtak.periode.tilOgMedDato == null || fraOgMedDato <= vedtak.periode.tilOgMedDato
             })
         }
+
+        if (medKodefilter != original) {
+            val størsteTilDatoOriginal = original.vedtak.maxOfOrNull { it.periode.tilOgMedDato ?: LocalDate.MIN }
+            val størsteTilDatoMedKodefilter = medKodefilter.vedtak.maxOfOrNull { it.periode.tilOgMedDato ?: LocalDate.MIN }
+            log.warn("Kodefilter er forskjellig fra SQL. Lengde: ${medKodefilter.vedtak.size} vs ${original.vedtak.size}. Største til dato: $størsteTilDatoMedKodefilter vs $størsteTilDatoOriginal.")
+        }
+        return original
     }
 
     suspend fun hentArenaSakMedVedtak(callId: String, sakId: String): ArenaSakMedVedtakResponse? =
@@ -172,8 +192,7 @@ private fun SakerResponse.toResponse() = ArenaSakerResponse(
             regDato = sak.regDato,
             avsluttetDato = sak.avsluttetDato,
         )
-    }
-)
+    })
 
 private fun ArenaSakMedVedtakResponseV1.toInternResponse() = ArenaSakMedVedtakResponse(
     sakId = sakId,
